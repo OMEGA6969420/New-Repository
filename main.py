8ab974ce89a11e130d984d5ec5cf00e0381fe2fe
OMEGA2189 <107874029+OMEGA6969420@users.noreply.github.com>
2024-05-17 22:58:14 -0600

    main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "Una respuesta a una broma",
+            "SHEESH": "ligera desaprobación",
+            "CREEPY": "aterrador, siniestro",
+            "AGGRO": "ponerse agresivo/enojado"
+            }
+            
+
+for i in range(5):
+    word = input("Buenos dias usuario, porfavor escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+    if word in meme_dict.keys():
+        print("Esa palabra es un meme")
+    else:
+        print("Esa palabra no es un meme")
